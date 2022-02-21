dc46382cea06c7ed0cb909bc3d8d69e6bf247614
Aleksander Roder <fylling8@gmail.com>
2022-02-21 23:46:00 +0100

    Added basic login page(expecting feide login)

---
+++ src/components/modulo/login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './login.css';
+
+export default function Login() {
+    return (
+        <div className="login-modulo">
+            <div className="feide-wrapper">
+                <div className="feide-login">
+                    <img srcSet="https://min.kunnskap.no/media/img/vertikal_feide4x.png" alt="Feide"/>
+                    <div className="arrow-wrapper"><h1 className="login-arrow">></h1></div>
+                </div>
+            </div>
+        </div>
+    )
+}
