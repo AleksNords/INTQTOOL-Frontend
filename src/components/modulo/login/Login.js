e816f341c46923450d8696d0a9972059b70db51d
Aleksander Roder <fylling8@gmail.com>
2022-03-16 18:18:17 +0100

    Remade login-page, and added support for backend authentication

--- src/components/modulo/login/Login.js
+++ src/components/modulo/login/Login.js
@@ -3,64 +3,71 @@ import './login.css';
 import {useDispatch, useSelector} from "react-redux";
 import {setUser} from "../../../store/action/userAction";
 import {setLoginStatus} from "../../../store/action/isLoggedAction";
-import {useNavigate,useLocation} from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [loginType, setLoginType] = React.useState("");
-    const [email,setEmail] = React.useState("");
-    const [code,setCode] = React.useState("");
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
     const isLogged = useSelector(state => state.isLoggedReducer);
     const user = useSelector(state => state.userReducer);
 
 
-    function submitLogin(){
-        let logginApproved = true;
-        if(logginApproved) {
-            dispatch(setUser({
-                email:email,
-            code: code}))
-            dispatch(setLoginStatus({
-                isLogged: true,
-                sessionToken: ""}))
-            navigate("/");
+    function submitLogin() {
+        var details = {
+            'username': username,
+            'password': password
+        };
 
+        let formBody = [];
+        for (let property in details) {
+            let encodedKey = encodeURIComponent(property);
+            let encodedValue = encodeURIComponent(details[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
         }
+        formBody = formBody.join("&");
+        axios({
+            method:'post',
+            url:"http://localhost:8080/login",
+            maxRedirects:0,
+            headers:{"Authorization":"none",
+            "Accept":"*/*",
+            "Content-Type":"application/x-www-form-urlencoded"},
+            data:formBody
+
+        }).then(function(response){
+            console.log(response);
+            console.log(response.status);
+            if(response.status === 200){
+                dispatch(setLoginStatus({
+                    isLogged: true,
+                    sessionToken: document.cookie
+                }))
+                navigate("/");
+            }
+        }).catch(function(response){
+            console.log(response.statusCode);
+            console.log(response);
+        });
     }
 
     return (
-        <div>
-            {loginType === "" ? (
-                <div className="login-modulo">
-                    <div className="login-wrapper">
-                        <div className="feide-login">
-                            <img srcSet="https://min.kunnskap.no/media/img/vertikal_feide4x.png" alt="Feide"/>
-                            <h1>Feide</h1>
-                            <div className="arrow-wrapper"><h1 className="login-arrow">></h1></div>
-                        </div>
-                    </div>
-                    <div className="login-wrapper" onClick={() => {
-                        setLoginType("guest")
-                    }}>
-                        <div className="feide-login">
-                            <h1>Guest</h1>
-                            <div className="arrow-wrapper"><h1 className="login-arrow">></h1></div>
-                        </div>
-                    </div>
-                </div>
-            ) : (loginType === "guest") ? (
-                <div className="guest-login">
-                    <div className="guest-login-fields">
-                        <label>Email:</label>
-                        <input type={"text"} onChange={(elem)=>setEmail(elem.target.value)}/>
-                        <label>Code:</label>
-                        <input type={"text"} onChange={(elem)=>setCode(elem.target.value)}/>
-                    </div>
-                    <input type={"button"} className={"login-submit-btn"} value={"Login"} onClick={submitLogin}/>
-                </div>
-            ) : null}
+
+        <div className="login-wrapper">
+            <div className={"login-fields"}>
+                <label>Username</label>
+                <input type={"text"} className={"login-form-field"} onChange={elem=>setUsername(elem.target.value)}/>
+                <label>Password</label>
+                <input type={"password"} className={"login-form-field"} onChange={elem=>setPassword(elem.target.value)}/>
+            </div>
+            <div className={"login-submit-wrapper"}>
+                <input type={"button"} value={"Login"} className={"login-form-submitButton"} onClick={submitLogin}/>
+                <p>Dont have an account? <a>Sign up here!</a></p>
+            </div>
+
         </div>
     )
 }
