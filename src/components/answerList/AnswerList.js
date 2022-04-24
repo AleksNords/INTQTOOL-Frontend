e09973619eb8424588c876f839cdb4b14a059692
Aleks <arbitalstrike@gmail.com>
2022-04-25 01:56:36 +0200

    Added communication with backend when grading quiz. Fixed bugs when switching currently graded question.

---
+++ src/components/answerList/AnswerList.js
@@ -0,0 +1,107 @@
+import React, {useEffect, useState} from 'react';
+import './answerlist.css';
+import {Fade, FormControlLabel, IconButton, Radio, RadioGroup, Slide, TextField} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import PercentIcon from "@mui/icons-material/Percent";
+import CommentIcon from '@mui/icons-material/Comment';
+import SendIcon from '@mui/icons-material/Send';
+import AnswerCard from "../answerCard/AnswerCard";
+import Snackbar from "@mui/material/Snackbar";
+import {Alert} from "@mui/lab";
+
+export default function AnswerList({answers, setCurrentAnswerFunction, currentQuestion}) {
+
+    const [search, setSearch] = useState("");
+    const [showToolBar, setShowToolbar] = useState(false);
+    const [showToolbarFeedback, setshowToolbarFeedback] = useState(false);
+    const [toolbarFeedback, setToolbarFeedback] = useState("");
+    const [checkedAnsAmnt, setCheckedAnsAmnt] = useState(0);
+    const [showAnswerGraded, setShowAnswerGraded] = useState(false);
+    const [checkedAnswers, setCheckedAnswers] = useState([]);
+
+    let answerDivs = [];
+
+    for (let i = 0; i < answers.length; i++) {
+        answerDivs.push(<AnswerCard setCurrentAnswerFunction={setCurrentAnswerFunction} answer={answers[i]} setCheckedFunction={setChecked}/>);
+    }
+
+    const handleCloseSnackbar = () => {
+        setShowAnswerGraded(false);
+    };
+
+    function setChecked(answerId, isChecked) {
+        if (isChecked) {
+            let temp = checkedAnswers;
+            temp.push(answers.find((ans) => ans.id === answerId));
+            setCheckedAnswers(temp);
+        } else {
+            let temp = checkedAnswers;
+            temp.splice(checkedAnswers.findIndex((ans) => ans.id === answerId), 1);
+            setCheckedAnswers(temp);
+        }
+
+        if (checkedAnswers.length > 0) {
+            setShowToolbar(true);
+        } else {
+            setShowToolbar(false);
+        }
+        setCheckedAnsAmnt(checkedAnswers.length);
+        console.log(checkedAnswers);
+    }
+
+    useEffect(()=> {
+        setCheckedAnswers([]);
+        setCheckedAnsAmnt(0);
+        setShowToolbar(false);
+        setSearch("");
+    },[answers])
+
+    return (
+        <div className="answer-list">
+            <Snackbar sx={{color: "white"}} open={showAnswerGraded} autoHideDuration={6000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} >
+                <Alert onClose={handleCloseSnackbar} severity="success" sx={{width: "100%", color: "white", backgroundColor: "#40aa5a", fontSize: 15, ".css-ptiqhd-MuiSvgIcon-root": {fontSize: 20}, ".MuiSvgIcon-root": {color: "white"} }}>
+                    Selected answer(s) have been graded!
+                </Alert>
+            </Snackbar>
+            <div className="answer-list-header">
+                <h1 key={answers + answers.length} className="answer-list-title answer-list-header-item">Student answers ({answers.length > 0 ? answers.length : 0})</h1>
+                <TextField className="answer-list-header-item"
+                           key={answers + currentQuestion}
+                           label={<div><SearchIcon sx={{fontSize: 20}}/>Search</div>}
+                           InputLabelProps={{style: {fontSize: 18}}} InputProps={{style: {fontSize: 18}}}
+                           value={search}
+                           onChange={(elem) =>{setSearch(elem.target.value)}}/>
+            </div>
+            {
+                answers.length > 0 ?
+                <span className="selected-amount">{checkedAnswers.length} selected </span>
+                    : null
+            }
+            <div className="answer-card-wrapper">
+                { search === "" ?
+                    answerDivs.map((answer) => answer)
+                  :
+                    answerDivs.filter((ans) => ans.props.answer.answer.toLowerCase().includes(search.toLowerCase())).map((answer) => answer)
+                }
+            </div>
+                <Slide direction="up" in={showToolBar} mountOnEnter unmountOnExit>
+                    <div className="grading-toolbar">
+                        <Fade in={showToolbarFeedback}>
+                            <TextField focused={true} inputRef={input => input && input.focus()} multiline rows={3} defaultValue={toolbarFeedback} onChange={(e) => setToolbarFeedback(e.target.value)} className="toolbar-feedback-textfield"/>
+                        </Fade>
+                        <RadioGroup className="credit-radio-wrapper toolbar-radio-wrapper" row>
+                            <FormControlLabel className="grading-button" value="correct" control={<Radio TouchRippleProps={{sx: {color: "#42c767"}}} className="grading-radio" icon={<CheckIcon sx={{fontSize: 40}}/>} checkedIcon={<CheckIcon sx={{fontSize: 40, color: "#42c767"}}/>} size={"large"}/>}/>
+                            <FormControlLabel className="grading-button" value="incorrect" control={<Radio TouchRippleProps={{sx: {color: "#F63E3E"}}} className="grading-radio" icon={<CloseIcon sx={{fontSize: 40}}/>} checkedIcon={<CloseIcon sx={{fontSize: 40, color: "#f63e3e"}}/>} size={"large"}/>}/>
+                            <FormControlLabel className="grading-button" value="partly_correct" control={<Radio TouchRippleProps={{sx: {color: "#F0C11B"}}} className="grading-radio" icon={<PercentIcon sx={{fontSize: 40}}/>} checkedIcon={<PercentIcon sx={{fontSize: 40, color: "#f0c11b"}}/>} size={"large"}/>}/>
+                        </RadioGroup>
+                        <div className="toolbar-button-wrapper">
+                            <IconButton onClick={() => setshowToolbarFeedback(!showToolbarFeedback)} TouchRippleProps={{sx: {color: "#2f7ed9"}}}><CommentIcon sx={{fontSize: 40, color: "#2f7ed9"}}/></IconButton>
+                            <IconButton onClick={() => setShowAnswerGraded(true)} TouchRippleProps={{sx: {color: "#42C767"}}}><SendIcon sx={{fontSize: 40, color: "#42C767"}}/></IconButton>
+                        </div>
+                    </div>
+                </Slide>
+        </div>
+    )
+}
---
+++ src/components/gradingQuiz/GradingQuiz.js
@@ -0,0 +1,96 @@
+import React, {useEffect, useState} from 'react';
+import './gradingquiz.css';
+import QuestionBanner from "../questionBanner/QuestionBanner";
+import axios from "axios";
+import {useParams} from "react-router";
+import {useSelector} from "react-redux";
+import GradingQuestion from "../gradingQuestion/GradingQuestion";
+import AnswerList from "../answerList/AnswerList";
+import {Alert} from "@mui/lab";
+import Snackbar from "@mui/material/Snackbar";
+
+export default function GradingQuiz() {
+
+    const isLogged = useSelector(state => state.isLoggedReducer);
+    const [quiz, setQuiz] = useState({});
+    const [course, setCourse] = useState("");
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [currentAnswer, setCurrentAnswer] = useState(0);
+    const url = "http://localhost:8080";
+    let {id} = useParams();
+
+    const [answers, setAnswers] = useState([])
+
+    useEffect(() => {
+
+        axios({
+            method: "get",
+            url: url+"/quiz/" + id,
+            headers: {
+                "Authorization": "Bearer " + isLogged.jwtToken
+            }
+        }).then(function (response) {
+            if (response.status === 200) {
+                let temp = response.data;
+                temp.deployedQuiz = JSON.parse(temp.deployedQuiz);
+                temp.deployedQuiz.questions = temp.deployedQuiz.questions.map((question) => JSON.parse(question));
+                setQuiz(temp);
+                getCourse();
+                getAnswers();
+            }
+            }
+        )
+    }, []);
+
+    function getAnswers() {
+        axios({
+            method: "get",
+            url: url +"/quiz/quizanswers/" + id,
+            headers: {
+                "Authorization": "Bearer " + isLogged.jwtToken
+            }
+        }).then(function (response) {
+            let temp = response.data;
+            temp = temp.map((answers) => answers.map((answer) => JSON.parse(answer)));
+            setAnswers(temp);
+            setCurrentAnswer(temp[0][0].id);
+        })
+    }
+
+    function getCourse() {
+        axios({
+            method: "get",
+            url: url +"/course/" + id,
+            headers: {
+                "Authorization": "Bearer " + isLogged.jwtToken
+            }
+        }).then(function (response) {
+            setCourse(response.data);
+        })
+    }
+
+    //function setCheckedFunction(answerId, newChecked) {
+    //    let temp = answers;
+    //    answers[answerId].checked = newChecked;
+    //    setAnswers(temp);
+    //}
+
+    return (
+        <div className="grading-quiz">
+            <QuestionBanner currentQuestion={currentQuestion} quizLength={quiz.deployedQuiz ? quiz.deployedQuiz.quizLength : undefined} setCurrentQuestion={(e)=>{setCurrentAnswer(answers[e][0].id);setCurrentQuestion(e)}}/>
+            <div className={"grading-wrapper"}>
+                {quiz.deployedQuiz && quiz.deployedQuiz.questions[currentQuestion].type === 1 ? <div className="auto-graded-question-filter">
+                    <Snackbar sx={{color: "white"}} open={quiz.deployedQuiz && quiz.deployedQuiz.questions[currentQuestion].type === 1} autoHideDuration={6000} anchorOrigin={{ vertical: 'center', horizontal: 'center' }} >
+                    <Alert severity="warning" sx={{width: "100%", color: "white", backgroundColor: "#c6c30e", fontSize: 15, ".css-ptiqhd-MuiSvgIcon-root": {fontSize: 20}, ".MuiSvgIcon-root": {color: "white"} }}>
+                        This answer is autograded!
+                    </Alert>
+                </Snackbar></div> : null}
+                <div className="feedback-wrapper">
+                    <h1 className="course-quiz-title">{course.name} > {quiz.deployedQuiz ? quiz.deployedQuiz.title : undefined}</h1>
+                    <GradingQuestion currentAnswer={answers[currentQuestion] && quiz.deployedQuiz.questions[currentQuestion].type !== 1 ? answers[currentQuestion].find((ans) => ans.id === currentAnswer) : undefined} questionIndex={currentQuestion + 1} question={quiz.deployedQuiz ? quiz.deployedQuiz.questions[currentQuestion] : undefined}/>
+                </div>
+                <AnswerList setCurrentAnswerFunction={setCurrentAnswer} currentQuestion={currentQuestion} answers={quiz.deployedQuiz && quiz.deployedQuiz.questions[currentQuestion].type === 2 ? answers[currentQuestion] : {}} question={quiz.deployedQuiz ? quiz.deployedQuiz.questions[currentQuestion] : undefined}/>
+            </div>
+        </div>
+    )
+}
